34fba5ce497a78ba0407a5af2f33f61dcd5e8381
“FelipeMPorto” <felipe423porto@gmail.com>
2022-01-12 15:07:26 -0300

    BootsTrap

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React from 'react';import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
